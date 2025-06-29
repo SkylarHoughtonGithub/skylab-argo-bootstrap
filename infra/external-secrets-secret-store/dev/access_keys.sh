09aeb71c11ed131157d95043f1c1a5d9652a0328
Skylar Houghton <skylarhoughtongithub@gmail.com>
2025-06-29 13:13:40 -0500

    updated namespace

--- infra/external-secrets-secret-store/dev/access_keys.sh
+++ infra/external-secrets-secret-store/dev/access_keys.sh
@@ -2,4 +2,4 @@
 kubectl create secret generic aws-credentials \
   --from-literal=access-key-id=YOUR_ACTUAL_ACCESS_KEY_ID \
   --from-literal=secret-access-key=YOUR_ACTUAL_SECRET_ACCESS_KEY \
-  --namespace=external-secrets-system
+  --namespace=external-secrets
