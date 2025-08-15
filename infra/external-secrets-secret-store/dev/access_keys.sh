2a3753ff7b4484dd066f7dd420a23d1dd70d2e53
Skylar Houghton <skylarhoughtongithub@gmail.com>
2025-08-15 13:51:14 -0500

    job to kustomize

--- infra/external-secrets-secret-store/dev/access_keys.sh
+++ infra/external-secrets-secret-store/dev/access_keys.sh
@@ -1,4 +1,4 @@
-#
+# seed external secrets to aws
 kubectl create secret generic aws-credentials \
   --from-literal=access-key-id=YOUR_ACTUAL_ACCESS_KEY_ID \
   --from-literal=secret-access-key=YOUR_ACTUAL_SECRET_ACCESS_KEY \
